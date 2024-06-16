a00aa57fbea3d5be4b7c8d7fed9828c3b70f9bf0
Lavelliane <jhurylastre432@gmail.com>
2024-06-16 21:57:13 +0800

    blank

--- src/app/(private)/home/page.jsx
+++ src/app/(private)/home/page.jsx
@@ -1,8 +1,6 @@
 'use client'
-import AnimeCards from "@/components/AnimeCards"
 import FeaturedBanner from "@/components/FeaturedBanner"
 import { animeSeries } from "@/dummyData/dummy"
-import { useRouter } from "next/navigation"
 
 const staticDataBanner = {
   imageSource: "anime-covers/frieren.jpg", 
@@ -16,19 +14,13 @@ const staticDataBanner = {
 }
 
 function HomePage() {
-  const router = useRouter()
-  const routeToDetails = (id) => {
-    router.push(`anime/${id}`)
-  }
   return (
     <div>
         <FeaturedBanner {...staticDataBanner} />
         <p className="text-3xl font-bold mt-8 border-l-royal-purple border-l-8 pl-2">Anime</p>
         <p className="mt-2">Browse your favorite anime</p>
         <div className="flex gap-2 mt-4 justify-between">
-          {animeSeries.map((anime) => (
-            <AnimeCards image={anime.image} key={anime.id} title={anime.title} season={anime.season} onClick={() => routeToDetails(anime.id)}/>
-          ))}
+
         </div>
     </div>
   )
